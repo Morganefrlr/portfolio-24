c129ed0073fd16f2077b99e731d210bb2275b37b
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-04-04 10:32:22 +0200

    P01: add image/ layer homePage Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-
-
+import styled from "styled-components";
 
 function App() {
-  
-
-  return (
-    <div>Bonjour</div>
-  )
+  return <MainPageStyled>
+    <div className="left"></div>
+    <div className="center">dzfdfdzfdz</div>
+    <div className="right"></div>
+  </MainPageStyled>;
 }
 
-export default App
+const MainPageStyled = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  background-image: url("/fond.jpg");
+  background-size: cover;
+  color: aliceblue;
+
+  .left{
+    flex: 1;
+    background-color: aqua;
+  }
+  .center{
+    flex: 3;
+    background-color: red;
+  }
+  .right{
+    flex: 0.5;
+    background-color:green;
+  }
+
+
+`;
+
+export default App;
