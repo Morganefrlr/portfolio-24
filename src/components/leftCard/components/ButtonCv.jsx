8e0c734dfa0d9f0cd8305d662bcbffdf45116fbc
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-04-08 10:59:16 +0200

    P02: buttonCv styled

--- src/components/leftCard/components/ButtonCv.jsx
+++ src/components/leftCard/components/ButtonCv.jsx
@@ -1,14 +1,35 @@
 import { FaDownload } from "react-icons/fa";
+import styled from "styled-components";
 
 const ButtonCv = () => {
   return (
-    <div className="blocCV">
-      <div>
-        <FaDownload />
-        <p>Télécharger CV</p>
-      </div>
-    </div>
+    <ButtonCvStyled>
+      <FaDownload />
+      <p>Télécharger CV</p>
+    </ButtonCvStyled>
   );
 };
 
+const ButtonCvStyled = styled.div`
+  display: flex;
+  width: 100%;
+  height: 50px;
+  margin: 20px auto;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  color: #212121;
+  font-weight: 500;
+  background: linear-gradient(to right, #ce412e, #b60000);
+  cursor: pointer;
+  transition: all 500ms ease;
+  &:hover {
+    color: #fff;
+  }
+
+  &:active {
+    background: transparent;
+    border: 3px solid #b60000;
+  }
+`;
 export default ButtonCv;
