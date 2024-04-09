6c3f337beb521800bc256942ad4c8d577d6091ef
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-04-09 11:01:24 +0200

    P03: navbar refacto

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,126 +1,37 @@
-import { FaUser, FaDownload } from "react-icons/fa";
-import { FaToggleOff } from "react-icons/fa6";
-import { MdPushPin, MdWork } from "react-icons/md";
 import { BiSolidRightArrow } from "react-icons/bi";
 import styled from "styled-components";
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import AdminContext from "../../context/AdminContext";
+import { navbarMenuConfig } from "./navbarConfig";
+import ArrowMenu from "./components/ArrowMenu";
+import NavbarMenu from "./components/NavbarMenu";
 
 
 
 const Navbar = () => {
     const{menuSelected, setMenuSelected} = useContext(AdminContext)
-    const navbarPoint = [
-        {
-            label: "home",
-            icon: <FaUser/>
-        },
-        {
-            label: "service",
-            icon: <FaToggleOff/>
-        },
-        {
-            label: "expericence",
-            icon: <MdWork/>
-        },
-        {
-            label: "portfolio",
-            icon: < MdPushPin/>
-        },
-        {
-            label: "contact",
-            icon: <FaDownload/>
-        },
-    ]
+
+    const handleMenuSelected = (menu) =>{
+        setMenuSelected(menu)
+    }
 
     
     return (
         <NavbarStyled>
-            <div className="container">
-                <div className={`arrow ${menuSelected}`}>
-                    <BiSolidRightArrow className="icon"/>
-                </div>
-                <div className="box">
-                    {navbarPoint.map(item =>
-                        <div key={item.label} className={menuSelected === item.label ? 'button active' : 'button'} onClick={() => setMenuSelected(item.label)}>
-                            {item.icon}
-                        </div>
-                    )}
-                </div>
-                </div>
+            <ArrowMenu menuSelected={menuSelected}/>
+            <NavbarMenu menuSelected={menuSelected} handleClick={handleMenuSelected} />
         </NavbarStyled>
     );
 };
 
 const NavbarStyled = styled.div`
     flex: 0.5;
-
-
-    .container{
-
-        display: flex;
-        height: fit-content;
-        margin: 10vh auto;
-        gap: 5px;
-        position: relative;
-       
-    }
-
-    .arrow{
-        color: #B60000;
-        position: absolute;
-        transition: all 300ms ease;
-        left: -3px;
-        &.home{
-            top: 7%;
-        }
-        &.service{
-            top: 27%;
-        }
-        &.expericence{
-            top: 47%;
-        }
-        &.portfolio{
-            top: 67%;
-        }
-        &.contact{
-            top: 87%;
-        }
-    }
-
-    .box{
-        display: flex;
-        flex-direction: column;
-        width: fit-content;
-        height: fit-content;
-        gap: 5px;
-        margin-left: 25px;
-       
-    }
-    .button{
-        width: 50px;
-        height: 50px;
-        background: #191919;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        border-radius: 50%;
-        font-size: 12px;
-        cursor: pointer;
-        transition: all 300ms ease;
-
-        &:hover{
-            font-size: 16px;
-        }
-
-    }
-
-    .active{
-        background: linear-gradient(to right, #ce412e, #B60000);
-        color: #191919;
-        font-size: 14px;
-
-    }
+    display: flex;
+    height: fit-content;
+    margin: 10vh auto;
+    gap: 5px;
+    position: relative;
+    
 `
 
 export default Navbar;
---
+++ src/components/navbar/components/NavbarMenu.jsx
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+import { navbarMenuConfig } from "../navbarConfig";
+
+const NavbarMenu = ({menuSelected, handleClick}) => {
+    return (
+        <NavbarMenuStyled>
+            {navbarMenuConfig.map(item =>
+                <div key={item.label} 
+                    className={menuSelected === item.label ? 'menuButton menuButtonActive' : 'menuButton'} 
+                    onClick={() => handleClick(item.label)}>
+                    {item.icon}
+                </div>
+            )}
+        </NavbarMenuStyled>
+    );
+};
+
+const NavbarMenuStyled = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: fit-content;
+    height: fit-content;
+    gap: 5px;
+    margin-left: 25px;
+
+    .menuButton{
+        width: 50px;
+        height: 50px;
+        background: #191919;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 50%;
+        font-size: 12px;
+        cursor: pointer;
+        transition: all 300ms ease;
+
+        &:hover{
+            font-size: 16px;
+        }
+
+    }
+
+    .menuButtonActive{
+        background: linear-gradient(to right, #ce412e, #B60000);
+        color: #191919;
+        font-size: 14px;
+
+    }
+`
+
+export default NavbarMenu;
