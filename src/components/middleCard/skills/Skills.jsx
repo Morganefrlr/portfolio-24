4c6a1e0a27f9411cab89c68862571e771b2750ef
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-04-13 11:28:58 +0200

    P05: config skill

--- src/components/middleCard/skills/Skills.jsx
+++ src/components/middleCard/skills/Skills.jsx
@@ -1,6 +1,124 @@
 
 
 const Skills = () => {
+
+    const skillLang = [
+        {
+            label: 'HTML',
+            icon: '/logo/html.png',
+            couleur:'#fc490b',
+            number: 90,
+        },
+        {
+            label: 'Javascript',
+            icon: '/logo/js.png',
+            couleur:'#f0db4f',
+            number: 70,
+        },
+        {
+            label: 'ReactJs',
+            icon: '/logo/react.png',
+            couleur:'#01d8ff',
+            number: 80,
+        },
+        {
+            label: 'NextJs',
+            icon: '/logo/next.png',
+            couleur:'#000000',
+            number: 60,
+        },
+        {
+            label: 'Typescript',
+            icon: '/logo/ts.png',
+            couleur:'#3178c6',
+            number: 60,
+        },
+        {
+            label: 'NodeJs',
+            icon: '/logo/nodejs.png',
+            couleur:'#539e43',
+            number: 70,
+        },
+    
+    ]
+    const skillStyle = [
+        {
+            label: 'Css',
+            icon: '/logo/css.png',
+            couleur:'#2196f3',
+            number: 100,
+        },
+        {
+            label: 'Sass',
+            icon: '/logo/sass.png',
+            couleur:'#cc6699',
+            number: 100,
+        },
+        {
+            label: 'TailwindCss',
+            icon: '/logo/tail.png',
+            couleur:'#06b6d4',
+            number: 100,
+        },
+        {
+            label: 'Styled Components',
+            icon: '/logo/styled.png',
+            couleur:'#e1a478',
+            number: 60,
+        },
+    
+    ]
+
+    const skillBd = [
+        {
+            label: 'MongoDb',
+            icon: '/logo/mongodb.png',
+            couleur:'#419732',
+            number: 70,
+        },
+        {
+            label: 'MySQL',
+            icon: '/logo/mysql.png',
+            couleur:'#4479a1',
+            number: 70,
+        }
+    ]
+
+    const skillOther = [
+        {
+            label: 'Git',
+            icon: '/logo/git.png',
+            couleur:'#f05033',
+            number: 70,
+        },
+        {
+            label: 'VsCode',
+            icon: '/logo/vs.png',
+            couleur:'#2e81b9',
+            number: 100,
+        },
+        {
+            label: 'Photoshop',
+            icon: '/logo/ps.png',
+            couleur:'#31a8ff',
+            number: 80,
+        },
+        {
+            label: 'Indesign',
+            icon: '/logo/id.png',
+            couleur:'#ff0050',
+            number: 100,
+        },
+        {
+            label: 'Figma',
+            icon: '/logo/figma.png',
+            couleur:'#f44e1c',
+            number: 80,
+        },
+    
+    ]
+
+
     return (
         <div>
             je suis skills
