2f7d5e167fdb57baacfa10bcb4e193dd8bf03c20
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-04-17 15:37:46 +0200

    P06: html expe component

--- src/components/middleCard/experience/Experience.jsx
+++ src/components/middleCard/experience/Experience.jsx
@@ -1,9 +1,90 @@
-import React from 'react';
+
 
 const Experience = () => {
     return (
         <div>
-            je suis l'expe
+            <div className="divFrmation">
+
+                <h1>Formations</h1>
+                <hr />
+
+                <div>
+                    <div>
+                        <p>2020-2022</p>
+                        <h3>developpeuse</h3>
+                    </div>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis quis eius eveniet quod laboriosam voluptate omnis quas, minima adipisci nulla corporis ipsam dolores ipsa veritatis praesentium. Est tempore perferendis sit!
+                    Rem vel, autem ad maxime facere quae a culpa dignissimos, ipsam itaque aliquid obcaecati consectetur porro? Explicabo, dolore veritatis, culpa atque perferendis necessitatibus qui veniam molestias fugiat suscipit dolores? </p>
+                </div>
+                <div>
+                    <div>
+                        <p>2020-2022</p>
+                        <h3>developpeuse</h3>
+                    </div>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis quis eius eveniet quod laboriosam voluptate omnis quas, minima adipisci nulla corporis ipsam dolores ipsa veritatis praesentium. Est tempore perferendis sit!
+                    Rem vel, autem ad maxime facere quae a culpa dignissimos, ipsam itaque aliquid obcaecati consectetur porro? Explicabo, dolore veritatis, culpa atque perferendis necessitatibus qui veniam molestias fugiat suscipit dolores? </p>
+                </div>
+                <div>
+                    <div>
+                        <p>2020-2022</p>
+                        <h3>developpeuse</h3>
+                    </div>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis quis eius eveniet quod laboriosam voluptate omnis quas, minima adipisci nulla corporis ipsam dolores ipsa veritatis praesentium. Est tempore perferendis sit!
+                    Rem vel, autem ad maxime facere quae a culpa dignissimos, ipsam itaque aliquid obcaecati consectetur porro? Explicabo, dolore veritatis, culpa atque perferendis necessitatibus qui veniam molestias fugiat suscipit dolores? </p>
+                </div>
+                <div>
+                    <div>
+                        <p>2020-2022</p>
+                        <h3>developpeuse</h3>
+                    </div>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis quis eius eveniet quod laboriosam voluptate omnis quas, minima adipisci nulla corporis ipsam dolores ipsa veritatis praesentium. Est tempore perferendis sit!
+                    Rem vel, autem ad maxime facere quae a culpa dignissimos, ipsam itaque aliquid obcaecati consectetur porro? Explicabo, dolore veritatis, culpa atque perferendis necessitatibus qui veniam molestias fugiat suscipit dolores? </p>
+                </div>
+
+            </div>
+            <hr />
+
+            <div className="divexpe">
+            <h1>Expériences</h1>
+                <hr />
+
+                <div>
+                    <div>
+                        <div>1</div>
+                        <p>2024-now...;</p>
+                    </div>
+                    <div>
+                        <h4>Developpeur full stack</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad eligendi laboriosam rem id animi illo quod fuga nulla architecto temporibus. Dignissimos labore, ipsam qui consequuntur perferendis esse quis deleniti cumque.
+                        Voluptas omnis iste earum fugiat debitis architecto quam explicabo, libero distinctio magnam.</p>
+                    </div>
+                </div>
+
+                <div>
+                    <div>
+                        <div>1</div>
+                        <p>2024-now...;</p>
+                    </div>
+                    <div>
+                        <h4>Developpeur full stack</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad eligendi laboriosam rem id animi illo quod fuga nulla architecto temporibus. Dignissimos labore, ipsam qui consequuntur perferendis esse quis deleniti cumque.
+                        Voluptas omnis iste earum fugiat debitis architecto quam explicabo, libero distinctio magnam.</p>
+                    </div>
+                </div>
+
+                <div>
+                    <div>
+                        <div>1</div>
+                        <p>2024-now...;</p>
+                    </div>
+                    <div>
+                        <h4>Developpeur full stack</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad eligendi laboriosam rem id animi illo quod fuga nulla architecto temporibus. Dignissimos labore, ipsam qui consequuntur perferendis esse quis deleniti cumque.
+                        Voluptas omnis iste earum fugiat debitis architecto quam explicabo, libero distinctio magnam.</p>
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 };
