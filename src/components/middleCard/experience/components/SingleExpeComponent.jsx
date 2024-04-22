836e0b8e8162424fb378418d7195ac3a357ae292
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-04-22 11:28:39 +0200

    P06: SingleExpeComponentStyled

--- src/components/middleCard/experience/components/SingleExpeComponent.jsx
+++ src/components/middleCard/experience/components/SingleExpeComponent.jsx
@@ -1,20 +1,69 @@
+import styled from "styled-components";
 
 
 const SingleExpeComponent = () => {
     return (
-        <div>
-            <div>
+        <SingleExpeComponentStyled>
+            <div className="test1">
                 <div>1</div>
                 <p>2024-now...;</p>
             </div>
-            <div>
+            <div className="test2">
                 <h4>Developpeur full stack</h4>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad eligendi laboriosam rem id animi illo quod fuga nulla architecto temporibus. Dignissimos labore, ipsam qui consequuntur perferendis esse quis deleniti cumque.
                 Voluptas omnis iste earum fugiat debitis architecto quam explicabo, libero distinctio magnam.</p>
             </div>
-        </div>
+        </SingleExpeComponentStyled>
 
     );
 };
+
+const SingleExpeComponentStyled = styled.div`
+    display: flex; 
+    flex-direction: column;
+    margin: 40px 0;
+    gap: 20px;
+    .test1{
+        display: flex;
+        width: 70%;
+        height: 40px;
+        background: #212120;
+        align-items: center;
+        position: relative;
+        div{
+            width: 45px;
+            height: 45px;
+            background: linear-gradient(to right, #ce412e, #B60000);
+            border: 2px solid #212120;
+            border-radius: 25px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 10px;
+            font-weight: 600;
+            position: absolute;
+            left: -10px;
+        }
+
+        p{
+            margin-left: 50px;
+        }
+    }
+
+    .test2{
+        margin-left: 50px;
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        width: 75%;
+
+        p{
+            font-size: 14px;
+            font-weight: 300;
+            line-height: 2;
+        }
+    }
+
+`
 
 export default SingleExpeComponent;
