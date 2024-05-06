4d11f91c0e77e6c5fd7b21e7a80bf198960a96f3
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-05-06 15:27:49 +0200

    P09: contact html

--- src/components/middleCard/contact/Contact.jsx
+++ src/components/middleCard/contact/Contact.jsx
@@ -1,10 +1,39 @@
 import styled from 'styled-components';
+import BoxInfos from '../../leftCard/components/BoxInfos';
 
 const Contact = () => {
     return (
         <ContactStyled>
             <h1>Contact</h1>
             <hr />
+            <div className='contactContainer'>
+                <div className='left'>
+                    <div className='input'>
+                        <p>Nom Complet</p>
+                        <input type="text" placeholder='Votre nom'/>
+                    </div>
+                    <div className='input'>
+                        <p>Adresse mail</p>
+                        <input type="email" placeholder='Votre email'/>
+                    </div>
+                    <div className='input'>
+                        <p>Sujet du Message</p>
+                        <input type="text" placeholder='Sujet'/>
+                    </div>
+                    <div className='input'> 
+                        <p>Message</p>
+                        <textarea placeholder='Sujet'></textarea>
+                    </div>
+                    <button>Envoyer</button>
+                </div>
+                <div className='right'>
+                    <div>
+                        <h4>Morgane Ancelin</h4>
+                        <p>Developpeuse React Junior</p>
+                    </div>
+                    <BoxInfos />
+                </div>
+            </div>
         </ContactStyled>
     );
 };
@@ -24,6 +53,19 @@ const ContactStyled = styled.div`
         width: 80%;
         margin: 20px auto;
         border: 0.3px solid  #323231ce;
+    }
+
+
+
+    .contactContainer{
+        display: flex;
+
+        .left{
+            flex: 1;
+        }
+        .right{
+            flex: 1;
+        }
     }
 `
 export default Contact;
