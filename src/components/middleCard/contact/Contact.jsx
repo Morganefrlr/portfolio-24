9fe77b3b4d55deaded206bfb80d93dabac9a13f2
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-05-06 16:05:24 +0200

    P09: contact styled

--- src/components/middleCard/contact/Contact.jsx
+++ src/components/middleCard/contact/Contact.jsx
@@ -8,28 +8,30 @@ const Contact = () => {
             <hr />
             <div className='contactContainer'>
                 <div className='left'>
-                    <div className='input'>
-                        <p>Nom Complet</p>
-                        <input type="text" placeholder='Votre nom'/>
-                    </div>
-                    <div className='input'>
-                        <p>Adresse mail</p>
-                        <input type="email" placeholder='Votre email'/>
-                    </div>
-                    <div className='input'>
-                        <p>Sujet du Message</p>
-                        <input type="text" placeholder='Sujet'/>
-                    </div>
-                    <div className='input'> 
-                        <p>Message</p>
-                        <textarea placeholder='Sujet'></textarea>
+                    <div className='container'>
+                        <div className='input'>
+                            <p>Nom Complet</p>
+                            <input type="text" placeholder='Votre nom'/>
+                        </div>
+                        <div className='input'>
+                            <p>Adresse mail</p>
+                            <input type="email" placeholder='Votre email'/>
+                        </div>
+                        <div className='input'>
+                            <p>Sujet du Message</p>
+                            <input type="text" placeholder='Sujet'/>
+                        </div>
+                        <div className='input'> 
+                            <p>Message</p>
+                            <textarea placeholder='Message'></textarea>
+                        </div>
                     </div>
                     <button>Envoyer</button>
                 </div>
                 <div className='right'>
-                    <div>
+                    <div className='title'>
                         <h4>Morgane Ancelin</h4>
-                        <p>Developpeuse React Junior</p>
+                        <p className='job'>Developpeuse React Junior</p>
                     </div>
                     <BoxInfos />
                 </div>
@@ -59,12 +61,94 @@ const ContactStyled = styled.div`
 
     .contactContainer{
         display: flex;
-
+        margin-top: 5vh;
         .left{
             flex: 1;
+           display: flex;
+           flex-direction: column;
+           gap: 20px;
+            .container{
+                display: flex;
+                flex-direction: column;
+                justify-content: space-between;
+                height: 26vh;
+            }
+            .input{
+                width: 90%;
+                border: 1px solid #323231ce;
+                border-radius: 10px;
+                position: relative;
+                padding: 16px;
+
+                p{
+                    font-size: 10px;
+                    text-transform: uppercase;
+                    width: fit-content;
+                    padding: 2px 6px;
+                    background-color: #2a2a29;
+                    position: absolute;
+                    top: -8px;
+                }
+                input, textarea{
+                    background-color: transparent;
+                    border: none;
+                    width: 100%;
+
+                    &::placeholder{
+                        font-size: 10px;
+                        color: #484846;
+                    }
+
+                    &:focus{
+                        outline: 1px solid #ce412e;
+                        border-radius: 10px;
+                        padding: 16px;
+                        
+                    }
+                }
+                
+            }
+
+            button{
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                width: 25%;
+                height: 50px;
+                border-radius: 10px;
+                border: none;
+                cursor: pointer;
+                gap: 10px;
+                font-weight: 500;
+                font-size: 15px;
+                background: linear-gradient(to right, #ce412e, #B60000);
+                &:active{
+                    background: linear-gradient(to left, #ce412e, #B60000);
+                }
+            }
         }
         .right{
-            flex: 1;
+            flex: 0.5;
+            height: 26vh;
+            border: 1px solid #323231ce;
+            display: flex;
+            flex-direction: column;
+            padding: 60px 0 0 30px;
+            border-radius: 10px;
+
+            .title{
+                display: flex;
+                flex-direction: column;
+                gap: 5px;
+            }
+            .job{
+                background: linear-gradient(to right, #ce412e, #B60000);
+                -webkit-background-clip: text;
+                -webkit-text-fill-color: transparent;
+                font-size: 10px;
+                font-weight: 600;
+                text-transform: uppercase;
+            }
         }
     }
 `
