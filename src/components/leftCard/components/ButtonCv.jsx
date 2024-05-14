48bac665664570fe6b62d58f831d11bc09b175da
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-05-14 10:44:31 +0200

    P10: buttoncv styled

--- src/components/leftCard/components/ButtonCv.jsx
+++ src/components/leftCard/components/ButtonCv.jsx
@@ -3,16 +3,16 @@ import styled from "styled-components";
 
 const ButtonCv = () => {
   return (
-    <ButtonCvStyled>
-      <a href="/cv.pdf" download="MoganeAncelinCv">
+    
+      <ButtonCvStyled href="/cv.pdf" download="MoganeAncelinCv">
         Télécharger CV
         <FaDownload />
-      </a> 
-    </ButtonCvStyled>
+      </ButtonCvStyled> 
+    
   );
 };
 
-const ButtonCvStyled = styled.div`
+const ButtonCvStyled = styled.a`
   display: flex;
   width: 100%;
   height: 50px;
@@ -21,6 +21,7 @@ const ButtonCvStyled = styled.div`
   align-items: center;
   gap: 10px;
   color:  #212120;
+  text-decoration: none;
   font-weight: 500;
   background: linear-gradient(to right, #ce412e, #b60000);
   cursor: pointer;
